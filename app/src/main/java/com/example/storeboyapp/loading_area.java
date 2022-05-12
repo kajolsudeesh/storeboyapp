dac7b69bdf207e546264c39df9bcbc61a1ffaaab
AmayaVS <amayavammu@gmail.com>
2022-05-12 18:53:49 +0530

    storeboy UI commit

--- app/src/main/java/com/example/storeboyapp/loading_area.java
+++ app/src/main/java/com/example/storeboyapp/loading_area.java
@@ -17,57 +17,59 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class searchmenu extends AppCompatActivity {
-    TextView textview;
+public class loading_area extends AppCompatActivity {
+    TextView textView;
     ArrayList<String> arrayList;
     Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_searchmenu);
-        textview = findViewById(R.id.txtview);
-        arrayList = new ArrayList();
+        setContentView(R.layout.activity_loading_area);
+
+        textView = findViewById(R.id.text_view);
+        arrayList = new ArrayList<>();
+
         arrayList.add("Minipalais A1102");
-        arrayList.add("Minipalais A1103");
-        arrayList.add("Minipalais A202");
-        arrayList.add("Minipalais A3104");
-        arrayList.add("Minipalais A4107");
-        arrayList.add("Minipalais A5110");
+        arrayList.add("Amayavs A1103");
+        arrayList.add("AnjanapsA202");
+        arrayList.add("kajol A3104");
+        arrayList.add("praveen A4107");
+        arrayList.add("nidhinA5110");
         arrayList.add("Minipalais A6112");
         arrayList.add("Minipalais A7124");
-        textview.setOnClickListener(new View.OnClickListener() {
+
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog = new Dialog(searchmenu.this);
+                dialog = new Dialog(loading_area.this);
                 dialog.setContentView(R.layout.dialog_searchable_spinner);
+
                 dialog.getWindow().setLayout(650, 800);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
-                EditText editText = dialog.findViewById(R.id.edittext);
-                ListView listView = dialog.findViewById(R.id.listview);
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(searchmenu.this, android.R.layout.simple_list_item_1, arrayList);
+                EditText editText = dialog.findViewById(R.id.edit_text);
+                ListView listView = dialog.findViewById(R.id.list_item);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(loading_area.this, android.R.layout.simple_list_item_1, arrayList);
                 listView.setAdapter(adapter);
+
                 editText.addTextChangedListener(new TextWatcher() {
                     @Override
                     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
                     }
-
                     @Override
                     public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                         adapter.getFilter().filter(charSequence);
                     }
-
                     @Override
                     public void afterTextChanged(Editable editable) {
-
                     }
                 });
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        textview.setText(adapter.getItem(i));
+                        textView.setText(adapter.getItem(i));
                         dialog.dismiss();
                     }
                 });
